9269f693c54d74dc332f5f48bced5a192b72245c
tearshark <tearshark@163.com>
2018-05-26 08:24:07 +0800

    create mac project

---
+++ mac_proj/libtask/main.cpp
@@ -0,0 +1,43 @@
+
+//
+//  main.cpp
+//  libtask
+//
+//  Created by tearshark on 2018/5/26.
+//  Copyright © 2018年 tearshark. All rights reserved.
+//
+
+#include "task.h"
+#include "task_context.h"
+
+namespace lib_shark_task
+{
+    immediate_task_context imm_context;
+    async_task_context async_context;
+}
+
+void test_task_link();
+void test_task_async();
+void test_task_optimal_move();
+void test_task_conflict();
+void test_task_exception();
+void test_task_marshal();
+
+void test_task_thread_safe();
+void test_task_when_all();
+void test_task_when_any();
+
+int main(int argc, const char * argv[])
+{
+    //test_task_link();
+    //test_task_async();
+    //test_task_optimal_move();
+    ////test_task_conflict();
+    //test_task_exception();
+    //test_task_marshal();
+    //test_task_thread_safe();
+    //test_task_when_all();
+    test_task_when_any();
+    
+    return 0;
+}
